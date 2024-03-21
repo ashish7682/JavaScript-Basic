6ddcb38a40f9971221566d74094f6a572949cfe3
Ashish Kumar Sahoo <ashishkumarsahoo2023@gmail.com>
2024-03-22 00:28:35 +0530

    javaScript basic pdf

---
+++ TimeBased_Event.js
@@ -0,0 +1,28 @@
+/* 👉         JavaScript Timing Events
+
+      *********************************************
+
+
+ The window object allows execution of code at specified time intervals.
+
+ These time intervals are called timing events.
+
+ The two key methods to use with JavaScript are:
+
+ setTimeout(function, milliseconds)
+ Executes a function, after waiting a specified number of milliseconds.
+
+ setInterval(function, milliseconds)
+ Same as setTimeout(), but repeats the execution of the function continuously.
+
+
+ 1️⃣ setTimeout()
+
+ 2️⃣clearTimeout()
+
+ 3️⃣ setInterval()
+
+ 4️⃣ clearInterval()..........*/
+
+
+
